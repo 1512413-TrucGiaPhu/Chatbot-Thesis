539e06336d0d919c79ee681e29e263a4056c1c7a
Trực Gia Phú - 1512413 <43942274+1512413-TrucGiaPhu@users.noreply.github.com>
2020-06-06 14:43:26 +0700

    add .gitignore

---
+++ Source/web-scraping/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI1: 'mongodb://giaphu:familigetsexy3@ds263917.mlab.com:63917/web-scraping',
+    mongoURI2: 'mongodb://giaphu:familigetsexy3@ds141108.mlab.com:41108/web-scraping2',
+    mongoURI3: 'mongodb://giaphu:familigetsexy3@ds231070.mlab.com:31070/web-scraping3',
+    mongoURITest: 'mongodb://test-user:abc123@ds149613.mlab.com:49613/test-database'
+  }
